8a85b642a45e77fad5d46b0d2149fb2c90a07903
Raunit Raj <Raunit-7549@users.noreply.github.com>
2026-08-07 13:50:48 +0000

    floor and ceil

---
+++ BinarySearch/FloorandCeil/Floor.java
@@ -0,0 +1,30 @@
+public class Floor {
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 20, 30, 40, 50 };
+        System.out.println(floor(arr, 25));
+    }
+
+    // It is done by using binary search.
+
+    public static int floor(int[] arr, int x){
+        
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = -1;
+
+        while(low <= high){
+
+            int mid = low + (high - low) / 2;
+
+            if(arr[mid] <= x){
+                ans = arr[mid];
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }       
+        return ans;
+
+    }
+}
